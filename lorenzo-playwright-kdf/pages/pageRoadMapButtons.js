a2f1e5d478520b0e1171557231e682322da4b802
baskarankrishnan <bkrishnan6@dedalus.eu>
2026-07-20 14:45:25 +0530

    Universal btn_Find //button[.//td[normalize-space()='Find']] matches both Clinics (td.Cmd_TT titled) + patient/caseload (td.Cmd_TTE untitled) Find buttons; validated DI=9 + CaseLoad 15->32

--- lorenzo-playwright-kdf/pages/pageRoadMapButtons.js
+++ lorenzo-playwright-kdf/pages/pageRoadMapButtons.js
@@ -1,4 +1,4 @@
 ﻿// pageRoadMapButtons
-export const btn_Find = "//button[@title='Find']";
+export const btn_Find = "//button[.//td[normalize-space()='Find']]";
 export const popup = "//p['Question - LORENZO']";
 export const lbl_popup = "//p['Question - LORENZO']";
