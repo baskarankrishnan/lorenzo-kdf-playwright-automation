03533ff0f6710c7c2c4c77605b551989975c9795
baskarankrishnan <bkrishnan6@dedalus.eu>
2026-07-21 07:25:47 +0530

    MSI: add btn_PopUpOk to pageRegConfirmationpopup (was only in pageHome); set Property=exists on all 11 verifyProperty steps missing it. Validated MSI 9->13

--- lorenzo-playwright-kdf/pages/pageRegConfirmationpopup.js
+++ lorenzo-playwright-kdf/pages/pageRegConfirmationpopup.js
@@ -2,6 +2,7 @@
 export const ico_Heading = "//p['Information - LORENZO']";
 export const lbl_ConfirmationTextTemp = "//div[@id='txtMessage']";
 export const btn_PopUpYes = "//td[normalize-space(.)='Yes']";
+export const btn_PopUpOk = "//td[@class='Cmd_TTE'][@title='Ok']";
 
 
 
